243040685fb3e07d1d5c730ecc00bcd0748d67c1
CzajaK-ZUT <82312878+CzajaK-ZUT@users.noreply.github.com>
2021-04-11 15:28:45 +0200

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,15 @@
+N = 9 
+
+kwota = 1000.00 
+
+Rata = kwota / N
+
+Konto = 0.00 
+
+for i in range(N):
+   konto=konto + rata 
+    
+print()
+print ('powinno być =', kwota)
+print ('stan konta   =', konto)
+             
